1db875ea576cb7d4288113ab73b88641d5a5501b
Pavel Balashov <pavel.balashov1@gmail.com>
2024-07-15 10:56:02 +0200

    Transform email-microservice into a microservice with TCP transport

--- apps/backend/email-microservice/src/main.ts
+++ apps/backend/email-microservice/src/main.ts
@@ -7,15 +7,21 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3001;
-  await app.listen(port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3001,
+      }
+    },
+  );
+  await app.listen();
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 email-microservice is running`
   );
 }
 
