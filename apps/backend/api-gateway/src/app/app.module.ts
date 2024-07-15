14bee455eb790d3ed456266ca751c87f749e8bd0
Pavel Balashov <pavel.balashov1@gmail.com>
2024-07-15 12:14:57 +0200

    Implement client methods in api-gateway

--- apps/backend/api-gateway/src/app/app.module.ts
+++ apps/backend/api-gateway/src/app/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      { name: 'EMAIL_MICROSERVICE', transport: Transport.TCP, options: { port: 3001 } },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/api-gateway/src/app/app.service.ts
+++ apps/backend/api-gateway/src/app/app.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { timeout } from 'rxjs/operators';
 
 @Injectable()
-export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+export class AppService implements OnApplicationBootstrap {
+  constructor(
+    @Inject('EMAIL_MICROSERVICE') private client: ClientProxy,
+  ) {}
+
+  async onApplicationBootstrap() {
+    // Delay application bootstrap until the client is connected
+    // Not mandatory, just for demonstration purposes
+    await this.client.connect();
+  }
+
+  getData() {
+    const pattern = { cmd: 'get-data' };
+    const payload = {};
+
+    type ResponseType = { message: string };
+    // Returns a hot Observable, must be subscribed to before it sends the message
+    return this.client.send<ResponseType>(pattern, payload).pipe(timeout(5000));
+  }
+
+  logData() {
+    const pattern = 'log-data';
+    const payload = 'Hello from the event pattern!';
+
+    // Returns a hot Observable, no need to subscribe to it - event is emitted immediately
+    this.client.emit(pattern, payload);
   }
 }
